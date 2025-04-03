51fb6c860b5f191ed56eea915da4940d47c488f8
waynemkearns <46266413+waynemkearns@users.noreply.github.com>
2025-04-03 09:20:37 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   output: 'export',
   distDir: 'out',
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
